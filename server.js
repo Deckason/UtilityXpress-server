8e3fa4e04e1388d24fc1b225e1c09348cfc6b933
Deckason <chibuikemchidera@gmail.com>
2024-02-14 16:23:38 +0100

    Created the controllers folder with userController file in it and using it to controll signing up of the user, Created the models folder with userModel file in it for modelling how the users data would look like, created the router folder with user.js file in it for specific routing for authentication routes. The backend API is now validating, signing and logging user in and also using jsonWebToken to help in authentication.

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const mongoose = require("mongoose")
+require("dotenv").config()
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const userRoute = require("./routes/user")
+
+const app = express()
+
+// Middlewares
+// app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
+
+// Routes
+app.use("/auth", userRoute)
+
+// Connect to DB
+mongoose.connect(process.env.MONGODB_URL)
+.then(()=>{
+    // Listen for requests
+    app.listen(process.env.PORT_NUMBER, ()=>{
+        console.log('server Connected to', process.env.PORT_NUMBER)
+    })
+}).catch(err=>{
+    console.log(err)
+})
